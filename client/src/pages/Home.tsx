f21616873639d728e20ff021638e16cec3774f51
suportecasadape <54271642-suportecasadape@users.noreply.replit.com>
2026-02-17 21:47:16 +0000

    Restored to 'e65de4e636c01d7dbf0f10f29f49ac1cabbb8e47'
    Replit-Restored-To: e65de4e636c01d7dbf0f10f29f49ac1cabbb8e47

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,11 +1,33 @@
+import PromoBar from "@/components/PromoBar";
 import Header from "@/components/Header";
-import CpfLookup from "@/components/CpfLookup";
+import SecurityBanner from "@/components/SecurityBanner";
+import HeroBanner from "@/components/HeroBanner";
+import TravelSearch from "@/components/TravelSearch";
+import PointsSection from "@/components/PointsSection";
+import ProductsCarousel from "@/components/ProductsCarousel";
+import DestinationsSection from "@/components/DestinationsSection";
+import Partners from "@/components/Partners";
+import ClubLivelo from "@/components/ClubLivelo";
+import AppDownload from "@/components/AppDownload";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-gray-50 flex flex-col" data-testid="home-page">
+    <div className="min-h-screen bg-white" data-testid="home-page">
+      <PromoBar />
       <Header />
-      <CpfLookup />
+      <SecurityBanner />
+      <HeroBanner />
+      <TravelSearch />
+      <div className="mt-8">
+        <PointsSection />
+      </div>
+      <ProductsCarousel />
+      <DestinationsSection />
+      <Partners />
+      <ClubLivelo />
+      <AppDownload />
+      <Footer />
     </div>
   );
 }
